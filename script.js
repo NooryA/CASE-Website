d11f19a161985e53780d552d7ac27815a0d8dc47
Noor Ahamed Sadique <apsarnoor@gmail.com>
2021-06-16 21:56:02 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,26 @@
+$("document").ready(function(){
+
+$("#about").hover(function(){
+
+$("#aboutContent").addClass("displayAboutContent");
+}, function(){
+$("#aboutContent").removeClass("displayAboutContent");
+
+
+});
+
+$("#aboutContent").hover(function(){
+
+    $("#aboutContent").addClass("displayAboutContent");
+    }, function(){
+    $("#aboutContent").removeClass("displayAboutContent");
+    
+    
+    });
+
+
+
+});
+
+
+
