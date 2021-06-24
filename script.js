f3c952bd98dd0e9fef5cba5d01fca5e581a43d5b
Noor Ahamed Sadique <apsarnoor@gmail.com>
2021-06-24 09:31:30 -0400

    Worked on hover images

--- script.js
+++ script.js
@@ -20,6 +20,52 @@ $("#aboutContent").hover(function(){
 
 
 
+$("#column1").hover(function(){
+
+$("#descriptionColumn1").stop().animate({
+    top:'65%',
+
+});
+
+$("#descriptionColumn1 p").css({"color": "white"});
+
+
+}, function(){
+    $("#descriptionColumn1").stop().animate({
+        top:'80%',
+    
+    });
+    $("#descriptionColumn1 p").css({"color": "black"});
+
+
+});
+
+
+
+
+
+$("#column2").hover(function(){
+
+    $("#descriptionColumn2").stop().animate({
+        top:'65%',
+    
+    });
+    
+    $("#descriptionColumn2 p").css({"color": "white"});
+    
+    
+    }, function(){
+        $("#descriptionColumn2").stop().animate({
+            top:'80%',
+        
+        });
+        $("#descriptionColumn2 p").css({"color": "black"});
+    
+    
+    });
+
+
+
 });
 
 
