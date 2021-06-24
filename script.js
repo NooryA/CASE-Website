4299fa08fbad9609e40be48fbf018f5a93aafb07
Noor Ahamed Sadique <apsarnoor@gmail.com>
2021-06-24 10:31:12 -0400

    finished with image hover second body - still haen't done media queries

--- script.js
+++ script.js
@@ -30,12 +30,15 @@ $("#descriptionColumn1").stop().animate({
 $("#descriptionColumn1 p").css({"color": "white"});
 
 
+// $("#descriptionColumn1 p").addClass(".showDescription")
+
+
 }, function(){
     $("#descriptionColumn1").stop().animate({
         top:'80%',
     
     });
-    $("#descriptionColumn1 p").css({"color": "black"});
+    $("#descriptionColumn1 p").css({"color": "rgb(141, 7, 250)"});
 
 
 });
@@ -59,11 +62,30 @@ $("#column2").hover(function(){
             top:'80%',
         
         });
-        $("#descriptionColumn2 p").css({"color": "black"});
+        $("#descriptionColumn2 p").css({"color": "rgb(0, 151, 252)"});
     
     
     });
 
+    $("#column3").hover(function(){
+
+        $("#descriptionColumn3").stop().animate({
+            top:'65%',
+        
+        });
+        
+        $("#descriptionColumn3 p").css({"color": "white"});
+        
+        
+        }, function(){
+            $("#descriptionColumn3").stop().animate({
+                top:'80%',
+            
+            });
+            $("#descriptionColumn3 p").css({"color": "rgb(255, 115, 0)"});
+        
+        
+        });
 
 
 });
